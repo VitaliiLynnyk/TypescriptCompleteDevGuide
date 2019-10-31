58a3bb1e2cacc2d31b6a8910a558fea7bfd822fc
VitaliiLynnyk <linnik2101@gmail.com>
2019-10-31 19:14:37 +0100

    repeat ts types

--- features/types.ts
+++ features/types.ts
@@ -1,2 +1,8 @@
 const today: Date = new Date();
 console.log(today.getDate());
+
+let point: { x: number; y: number } = { x: 10, y: 10 };
+
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
