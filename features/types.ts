bc0cca542461d5fda0c9d81659633e2e66559856
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-01 14:08:43 +0100

    learn about type annotations

--- features/types.ts
+++ features/types.ts
@@ -6,3 +6,47 @@ let point: { x: number; y: number } = { x: 10, y: 10 };
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function(a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+const logWeather = ({
+  date,
+  weather
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
