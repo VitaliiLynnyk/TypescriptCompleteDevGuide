d90d769a4070d192538ae5c1d8124f40bc0670ef
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-04 21:27:14 +0100

    add google map to html

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import { Company } from './Company';
-import { User } from './User';
+// import { Company } from './Company';
+// import { User } from './User';
 
-const user = new User();
-console.log(user);
+// const user = new User();
+// console.log(user);
 
-const company = new Company();
-console.log(company);
+// const company = new Company();
+// console.log(company);
+
+new google.maps.Map(document.querySelector('#map'), {
+  center: { lat: 0, lng: 0 },
+  zoom: 1
+});
