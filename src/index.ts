6c8f67681e55a0eaae60e13f21ee1280cae8e0a7
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-04 22:13:01 +0100

    add companyMarker

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
 const user = new User();
-
-// const company = new Company();
-// console.log(company);
+const company = new Company();
 
 const map = new CustomMap('map');
 map.addUserMarker(user);
+map.addCompanyMarker(company);
