9a821e9a3a17a54f7ff68b66cbbf0e3c3ae9da73
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-05 14:52:57 +0100

    add implamentation from Mappable to classes

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,8 @@
 import faker from 'faker';
 
-export class User {
+import { Mappable } from './Mappable';
+
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
