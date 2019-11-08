b3a6fecb2f665480b408349b44960e9dc7f976c9
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-08 21:41:10 +0100

    test sorting

--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -1,8 +1,17 @@
+import { LinkedList } from './LinkedList';
 import { Sorter } from './Sorter';
 import { NumbersCollections } from './NumbersCollections';
 import { CharactersCollection } from './CharactersCollection';
 
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(3);
+linkedList.add(-5);
+linkedList.add(8);
+linkedList.add(-3);
+linkedList.add(4);
+
 const numbersCollection = new CharactersCollection('trolololsabfdcbb');
-const sorter = new Sorter(numbersCollection);
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(sorter);
+console.log(linkedList.print);
