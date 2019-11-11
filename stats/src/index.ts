ae7a34eafe383e30085b0e658c03ed8b572a8660
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-11 17:24:25 +0100

    add Enum MatchResult

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
 
+import { MatchResult } from './Enums';
+
 const matches = fs
   .readFileSync('football.csv', {
     encoding: 'utf-8'
@@ -7,4 +9,12 @@ const matches = fs
   .split('\n')
   .map((row: string): string[] => row.split(','));
 
-console.log(matches);
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(manUnitedWins);
