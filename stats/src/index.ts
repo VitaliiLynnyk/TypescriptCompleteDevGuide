1a191f5abeb877463532c6886ac4ab32b9644680
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-11 18:20:03 +0100

    create CsvFileReader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,6 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-import { MatchResult } from './Enums';
+const matchs = new CsvFileReader('football.csv');
+matchs.read();
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
-
-let manUnitedWins = 0;
-
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(manUnitedWins);
+console.log(matchs.data);
