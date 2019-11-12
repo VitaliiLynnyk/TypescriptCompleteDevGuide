406c2dd976e010beafb7a9058f90cfb3de31a721
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-12 16:26:20 +0100

    test HtmlReport

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,16 +1,14 @@
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 import { MatchReader } from './MatchReader';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { CsvFileReader } from './CsvFileReader';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
