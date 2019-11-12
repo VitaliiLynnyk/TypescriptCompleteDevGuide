67f84150f40e6c8cbf5edb6368e4c3210cce779d
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-12 16:05:28 +0100

    add WinsAnalysis class

---
+++ stats/src/analyzers/WinsAnalysis.ts
@@ -0,0 +1,19 @@
+import { Analyzer } from '../Summary';
+import { MatchData } from '../MatchData';
+import { MatchResult } from '../Enums';
+
+export class WinsAnalysis implements Analyzer {
+  constructor(public team: string) {}
+
+  run(matches: MatchData[]): string {
+    let wins = 0;
+
+    for (let match of matches) {
+      if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        wins++;
+      }
+    }
+
+    return `Team ${this.team} won ${wins} games`;
+  }
+}
