68e8ca2160d03e9c6ca1e60480307a3e5618cc69
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-12 16:21:29 +0100

    test Analysis in index.ts

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,8 +1,16 @@
+import { Summary } from './Summary';
 import { MatchReader } from './MatchReader';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { CsvFileReader } from './CsvFileReader';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-console.log(matchReader.matches);
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
