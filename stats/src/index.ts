8388001dc95bd7731870db561a1d2d3494e6bef3
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-13 17:20:48 +0100

    practise generics

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -2,8 +2,7 @@ import { Summary } from './Summary';
 import { MatchReader } from './MatchReader';
 
 const matchReader = MatchReader.fromCsv('football.csv');
-matchReader.load();
-
 const summary = Summary.winsAnalysisWithConsoleReport('Man United');
 
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
