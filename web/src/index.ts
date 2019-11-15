ac16bc7db53cc0b10483600cec3afa41863e3072
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-15 21:14:15 +0100

    add set method to User

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,3 +3,5 @@ import { User } from './models/User';
 const user = new User({ name: 'myUser', age: 10 });
 
 console.log(user.get('age'));
+console.log(user.set({ age: 999 }));
+console.log(user.get('age'));
