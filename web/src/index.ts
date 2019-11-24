d4c8b59d3f320e826b05b3d325aab2e17b4c0d7d
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-24 22:00:49 +0100

    fix methods to arrow funct

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,8 +2,4 @@ import { User } from './models/User';
 
 const user = new User({ id: 1, name: 'Name', age: 20 });
 
-user.events.on('change', () => {
-  console.log('change');
-});
-
-user.events.trigger('change');
+console.log(user.get('name'));
--- web/src/models/Eventing.ts
+++ web/src/models/Eventing.ts
@@ -3,13 +3,13 @@ import { Callback, Events } from '../types';
 export class Eventing {
   events: Events = {};
 
-  on(eventName: string, callback: Callback): void {
+  on = (eventName: string, callback: Callback): void => {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
     this.events[eventName] = handlers;
-  }
+  };
 
-  trigger(eventName: string): void {
+  trigger = (eventName: string): void => {
     const handlers = this.events[eventName];
     if (!handlers || handlers.length === 0) {
       return;
@@ -18,5 +18,5 @@ export class Eventing {
     handlers.forEach(callback => {
       callback();
     });
-  }
+  };
 }
