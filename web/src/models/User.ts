059457bb354731cc1aad4198115c3769359ebbb1
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-24 19:27:16 +0100

    create Attribute,Sync  refactore User

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,41 +1,17 @@
 import axios, { AxiosResponse } from 'axios';
 
+import { Sync } from './Sync';
 import { Eventing } from './Eventing';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   age?: number;
   name?: string;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
   public events: Eventing = new Eventing();
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((res: AxiosResponse): void => {
-        this.set(res.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
-  }
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 }
