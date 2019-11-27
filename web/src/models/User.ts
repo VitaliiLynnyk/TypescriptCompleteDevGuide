aa63337436a50b1ed1ea9a8b507ca8bf09e58b44
VitaliiLynnyk <linnik2101@gmail.com>
2019-11-27 16:40:15 +0100

    add static method buildUserCollection to User

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,6 +1,7 @@
 import { Model } from './Model';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 import { Attributes } from './Attributes';
 
 export interface UserProps {
@@ -17,6 +18,12 @@ export class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
+    );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
     );
   }
 }
