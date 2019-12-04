a2e0b50955442552eb76c95169fa2d5dc83f6d40
VitaliiLynnyk <linnik2101@gmail.com>
2019-12-04 19:41:17 +0100

    add controller, get route and install metadata

--- server/src/controllers/loginController.ts
+++ server/src/controllers/loginController.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
 
+import { get } from './decorators/routes';
+import { controller } from './decorators/controller';
+
+@controller('/auth')
 class LoginController {
+  @get('/login')
   getLogin(req: Request, res: Response): void {
     res.send(
       `
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
+import './controllers/loginController';
 import { router } from './routes/loginRoutes';
+import { router as controllerRouter } from './controllers/decorators/controller';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['lol'] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
