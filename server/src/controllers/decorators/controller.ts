d295ae88c3357067f6b50acec5a13a30df979300
VitaliiLynnyk <linnik2101@gmail.com>
2019-12-05 20:52:18 +0100

    add Methods enum && fix method type in controller

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 
+import { Methods } from './Methods';
 import { AppRouter } from '../../AppRouter';
 
 export function controller(routePrefix: string) {
@@ -9,9 +10,14 @@ export function controller(routePrefix: string) {
     for (const key in target.prototype) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata('path', target.prototype, key);
+      const method: Methods = Reflect.getMetadata(
+        'method',
+        target.prototype,
+        key
+      );
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
       }
     }
   };
