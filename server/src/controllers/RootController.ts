0c913d017e43c46b81c66abbdaa4da29c6aa48f7
VitaliiLynnyk <linnik2101@gmail.com>
2019-12-06 21:35:24 +0100

    fix protected route & delete routes dir

--- server/src/controllers/RootController.ts
+++ server/src/controllers/RootController.ts
@@ -1,6 +1,16 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-import { controller, get } from './decorators';
+import { controller, get, use } from './decorators';
+
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not permitted');
+}
 
 controller('');
 class RootController {
@@ -21,5 +31,11 @@ class RootController {
       </div>
     `);
     }
+  }
+
+  @get('/protected')
+  @use(requireAuth)
+  getProtected(req: Request, res: Response) {
+    res.send('Welcome to protected route');
   }
 }
