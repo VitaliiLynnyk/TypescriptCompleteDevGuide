cc1bdd6d20f22bf087f528e83ec2f4f90bddf233
VitaliiLynnyk <linnik2101@gmail.com>
2019-12-06 21:03:26 +0100

    add test logger middleware

--- server/src/controllers/loginController.ts
+++ server/src/controllers/loginController.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express';
 
-import { get, controller } from './decorators';
+import { NextFunction } from 'connect';
+import { get, controller, use } from './decorators';
+
+function logger(req: Request, res: Response, next: NextFunction) {
+  console.log('req was made !');
+  next();
+}
 
 @controller('/auth')
 class LoginController {
   @get('/login')
+  @use(logger)
   getLogin(req: Request, res: Response): void {
     res.send(
       `
