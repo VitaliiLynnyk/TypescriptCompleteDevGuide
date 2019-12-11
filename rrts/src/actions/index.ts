b64271a6093bf32e027d56b0ecc09e3d01e7892f
VitaliiLynnyk <linnik2101@gmail.com>
2019-12-11 22:03:54 +0100

    fix fetchTodos types

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -2,14 +2,27 @@ import axios from 'axios';
 
 import { Dispatch } from 'redux';
 
+import { ActionTypes } from './types';
+
+interface Todo {
+  id: number;
+  title: string;
+  complated: boolean;
+}
+
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const res = await axios.get(url);
+    const res = await axios.get<Todo[]>(url);
 
-    dispatch({
-      type: 'FETCH_TODOS',
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
       payload: res.data
     });
   };
