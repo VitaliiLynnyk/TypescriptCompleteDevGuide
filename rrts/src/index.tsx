567dd8b6243ddd48e0448e691ca7304a6368dd97
VitaliiLynnyk <linnik2101@gmail.com>
2019-12-11 21:08:40 +0100

    add initial store, reducers

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,42 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-interface AppProps {
-  color?: string;
-}
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-const App = (props: AppProps): JSX.Element => {
-  return <div> {props.color} </div>;
-};
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-// interface AppState {
-//   counter: number;
-// }
+const store = createStore(reducers, applyMiddleware(thunk));
 
-// class App extends React.Component<AppProps, AppState> {
-//   constructor(props: AppProps) {
-//     super(props);
-
-//     this.state = { counter: 0 };
-//   }
-
-//   onInc = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
-
-//   onDec = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onInc}> Inc </button>
-//         <button onClick={this.onDec}> Dec </button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
-
-ReactDOM.render(<App color="red" />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
